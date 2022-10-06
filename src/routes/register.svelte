d52c4c3e759a2c9d4c4adc261ea69db95406dda7
DariaEvtina <catbrat02@gamil.com>
2022-10-06 12:58:15 +0300

    valmis

--- src/routes/register.svelte
+++ src/routes/register.svelte
@@ -8,16 +8,15 @@
     let error = null;
     async function submit(event) {
         const response = await post(`auth/register`, { username, name, password });
-
-        if (response.user) {
-            $session.user = response.user;
+        error=responce.error();
+        if (response.id) {
             goto('/');
         }
     }
 </script>
 
 <svelte:head>
-    <title>Sign up • Conduit</title>
+    <title>Registrerimine projekt</title>
 </svelte:head>
 
 <div class="auth-page">
@@ -41,7 +40,10 @@
                     <fieldset class="form-group">
                         <input class="form-control form-control-lg" type="password" required placeholder="Password" bind:value={password}>
                     </fieldset><br>
-                    <button class="btn btn-lg btn-primary pull-xs-right">
+                    {#if password.length > 1 && password.length <6}
+                        <sup><div class="alert alert-danger" role="alert"> Password to short</div></sup>
+                    {/if}
+                    <button class="btn btn-lg btn-primary pull-xs-right" disabled="{password.length <6}">
                         Sign up
                     </button>
                 </form>
